c6226bb3414adf7dcd0808832663df3228e59b3c
deepti-sp <deepti.pyati1199@gmail.com>
2021-11-02 16:15:50 +0530

    BASIC_JS

---
+++ array_display.js
@@ -0,0 +1,5 @@
+let array = [4, 8, 3];
+array.sort();
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
---
+++ dict_studinfo.js
@@ -0,0 +1,9 @@
+var person = {
+    firstName: "Deepti",
+    lastName: "P",
+    USN: 07,
+    marks1: 85,
+    marks2: 80,
+    marks3: 90
+  };
+  console.log(person);
