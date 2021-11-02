2ca26366a70da631faac65ce0f2a2c681dc39d69
deepti-sp <deepti.pyati1199@gmail.com>
2021-11-02 15:59:54 +0530

    basic_js

---
+++ datatype.js
@@ -0,0 +1,28 @@
+var num = 20; 
+console.log("The value of the variable is: ",num)
+console.log("type of variable is : ",typeof(num));
+
+var str = 'welcome'; 
+console.log("The value of the string is: ",str)
+console.log("type of variable is : ",typeof(str));
+
+var boo = true;
+console.log("The value of the variable is: ",boo) 
+console.log("type of variable is : ",typeof(boo));
+
+var x;
+console.log("the value of variable x is: ",x);
+
+var y = null;
+console.log("type of y variable is : ",typeof(y));
+
+var z = 2.5;
+console.log("type of z variable is : ",typeof(z));
+
+var m = -500;
+console.log("type of m variable is : ",typeof(m));
+
+
+
+
+
